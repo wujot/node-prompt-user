d6343031d3d230c24d34ba94a8b46f38ccee792c
Wojciech Jeziorski <wojciech.wujot@gmail.com>
2019-02-05 20:38:41 +0100

    Add Custom Instructions

---
+++ program.js
@@ -0,0 +1,24 @@
+'use strict';
+
+process.stdin.setEncoding('utf-8');
+
+process.stdin.on('readable', function() {
+    
+    var input = process.stdin.read();
+
+    if (input !== null) {
+	    var instruction = input.toString().trim();
+	    switch (instruction) {
+			case '/exit':
+		    	process.stdout.write('Quitting app!\n');
+	        	process.exit();
+		    	break;
+		    case 'version':
+		    	console.log(process.env);
+		    	break;
+		  	default:
+		    	process.stderr.write('Wrong instruction!\n');
+		    	break;
+		}
+	}
+});
